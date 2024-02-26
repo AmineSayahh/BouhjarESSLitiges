6c52867a0d2a9802ce0ad12b7f075af69c0ebd56
Mohamed Amine Sayah <116640816+AmineSayahh@users.noreply.github.com>
2024-02-26 23:14:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,29 +4,23 @@ let totalAmount = 0; // Example initial total amount
 
 // Function to update the donation count and total amount
 function updateDonationStats() {
-    document.getElementById('donor-count').textContent = donationCount;
-    document.getElementById('total-amount').textContent = totalAmount;
-
+  document.getElementById("donor-count").textContent = donationCount;
+  document.getElementById("total-amount").textContent = totalAmount;
 }
-
-
-
-
-
 
 // Function to add a new donation
 function addDonation(name, amount) {
-    // Increment donation count
-    donationCount++;
-    // Add amount to total
-    totalAmount += amount;
-    // Update UI
-    updateDonationStats();
-    // Add new donor to the list
-    const donorList = document.getElementById('donor-list');
-    const newDonation = document.createElement('li');
-    newDonation.textContent = ` ${name} - ${amount} دينار`;
-    donorList.appendChild(newDonation);
+  // Increment donation count
+  donationCount++;
+  // Add amount to total
+  totalAmount += amount;
+  // Update UI
+  updateDonationStats();
+  // Add new donor to the list
+  const donorList = document.getElementById("donor-list");
+  const newDonation = document.createElement("li");
+  newDonation.textContent = ` ${name} - ${amount} دينار`;
+  donorList.appendChild(newDonation);
 }
 
 // Update the UI with initial values
@@ -35,40 +29,40 @@ updateDonationStats();
 // Example usage:
 // Add a new donation
 
-addDonation('ضياء المحمدي', 461);
-addDonation('ياسين العاشق', 250);
-addDonation('نضال العاشق', 200);
-addDonation('وجدي بوراوي', 160);
-addDonation('عمر رجب', 150);
-addDonation('عبد الله الصياح', 130);
-addDonation('رائد الديماسي', 100); // Example donation
-addDonation('علي الديماسي', 100);
-addDonation('إلياس بن يوسف', 100);
-addDonation('نبيل منصور', 100);
-addDonation('يسري بوكذابة', 100);
-addDonation('فارس الصياح', 100);
-addDonation('شادي إسماعيل الحجري', 100);
-addDonation('سامي بن عمر', 100);
-addDonation('حمزة بوزيد', 100);
-addDonation('محمد طاهر العاشق', 100);
-addDonation('صلاح شطير', 100);
-addDonation('ياسر بوصاع', 100);
-addDonation('أيمن الصياح', 100);
-addDonation('فرج العبيدي', 100);
-addDonation('نوري الصالحي', 80);
-addDonation('ياسين السويبقي ', 80);
-addDonation('محمد عزيز السويبقي', 60);
-addDonation('محمد أمين الصياح', 50);
-addDonation(' بوكذابة عمر ', 50);
-addDonation('محمد يزيد الديماسي', 50);
-addDonation('محمد أمين الفقيه', 50);
-addDonation('كريم بوزيد', 50);
-addDonation('نعيم بوزيد', 50);
-addDonation('بوزيد نور الدين ', 50);
-addDonation('محمد عزيز الزغيدي', 50);
-addDonation('محمد بن الناصر بوزيد ', 50);
-addDonation('ميرنا وميار شطير ', 50);
-addDonation('عمر شطير', 30);
-addDonation('حافظ قريديح', 20);
-addDonation('شادي بوزيد', 20);
-addDonation('محمد ياسين بن يوسف', 20);
+addDonation("ضياء المحمدي", 461);
+addDonation("ياسين العاشق", 250);
+addDonation("نضال العاشق", 200);
+addDonation("وجدي بوراوي", 160);
+addDonation("عمر رجب", 150);
+addDonation("عبد الله الصياح", 130);
+addDonation("رائد الديماسي", 100); // Example donation
+addDonation("علي الديماسي", 100);
+addDonation("إلياس بن يوسف", 100);
+addDonation("نبيل منصور", 100);
+addDonation("يسري بوكذابة", 100);
+addDonation("فارس الصياح", 100);
+addDonation("شادي إسماعيل الحجري", 100);
+addDonation("سامي بن عمر", 100);
+addDonation("حمزة بوزيد", 100);
+addDonation("محمد طاهر العاشق", 100);
+addDonation("صلاح شطير", 100);
+addDonation("ياسر بوصاع", 100);
+addDonation("أيمن الصياح", 100);
+addDonation("فرج العبيدي", 100);
+addDonation("نوري الصالحي", 80);
+addDonation("ياسين السويبقي ", 80);
+addDonation("محمد عزيز السويبقي", 60);
+addDonation("محمد أمين الصياح", 50);
+addDonation(" بوكذابة عمر ", 50);
+addDonation("محمد يزيد الديماسي", 50);
+addDonation("محمد أمين الفقيه", 50);
+addDonation("كريم بوزيد", 50);
+addDonation("نعيم بوزيد", 50);
+addDonation("بوزيد نور الدين ", 50);
+addDonation("محمد عزيز الزغيدي", 50);
+addDonation("محمد بن الناصر بوزيد ", 50);
+addDonation("ميرنا وميار شطير ", 50);
+addDonation("عمر شطير", 50);
+addDonation("حافظ قريديح", 20);
+addDonation("شادي بوزيد", 20);
+addDonation("محمد ياسين بن يوسف", 20);
